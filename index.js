eb8c49361081c7ac1e0af788d7105eff0475dc45
Michel Angelucci <michelangelucci@hotmail.com>
2022-10-15 09:41:01 -0300

    09. Finalização do efeito menu lateral

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const menu = document.querySelector('.menu-lateral')
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 })
+
+const linkMenu = document.querySelectorAll('.menu-lateral__link')
+
+function addListenerMouseOverOut(item) 
+{
+    item.addEventListener('mouseover', onMouseOverOut)
+    item.addEventListener('mouseout', onMouseOverOut)
+}
+
+function onMouseOverOut() 
+{
+    this.classList.toggle('menu-lateral__link--ativo')
+}
+
+linkMenu.forEach(addListenerMouseOverOut)
